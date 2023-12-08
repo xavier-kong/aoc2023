28c74756a88debc61a4aeebc519acc132f5c87b4
xavier-kong <kongxianchengxavier@gmail.com>
2023-12-08 23:42:10 +0800

    i have no idea

--- 04/part1.exs
+++ 04/part1.exs
@@ -1,17 +1,17 @@
 File.stream!("test.txt") 
   |> Stream.map(&String.trim/1)
   |> Stream.map(fn (line) ->
-  [_no, yes] = String.split(line, ":")
-  [nums, draw] = String.split(yes, " | ")
-  winners = String.split(nums, " ")
+      [_no, yes] = String.split(line, ":")
+      [nums, draw] = String.split(yes, " | ")
+      winners = Enum.filter(String.split(nums, " "), fn x -> x != "" end)
 
-  drew = String.split(draw, " ")
 
-  for num <- drew do
-    IO.puts '#{Enum.member?(winners, num)}'
-  end
-  
-  IO.puts "#{winners}" 
-  end
+      lucky = Enum.filter(
+        Enum.filter(String.split(draw, " "), fn x -> x != "" end), 
+        fn num -> Enum.member?(winners, num) 
+      end)
+
+      IO.inspect(length(lucky))
+    end
   )
   |> Stream.run
