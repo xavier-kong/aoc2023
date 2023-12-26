1b5f7bef5bbfb1c3c0b2abeec72c6de839dbbbe9
xavier-kong <kongxianchengxavier@gmail.com>
2023-12-26 13:35:31 +0800

    confused recursion

--- 04/part2.exs
+++ 04/part2.exs
@@ -1,16 +1,16 @@
 defmodule Main do
   def traverse(curr_index, end_index, cards, possible, cards_left) do
     if curr_index >= end_index do
-      IO.inspect("end")
-      IO.inspect(cards)
+      #IO.inspect("end")
+      #IO.inspect(cards)
       cards
     else
       if cards_left > 0 do
-        IO.puts "cards left #{cards_left} cards #{Enum.join(cards, " ")}"
+        #IO.puts "cards left #{cards_left} cards #{Enum.join(cards, " ")}"
         new_cards = Main.add_cards(curr_index + 1, Enum.at(possible, curr_index), cards)
-        traverse(curr_index, end_index, new_cards, possible, cards_left - cards_left)
+        traverse(curr_index, end_index, new_cards, possible, cards_left - 1)
       else
-        IO.puts "cards_left == 0 #{Enum.join(cards, " ")}"
+        #IO.puts "cards_left == 0 #{Enum.join(cards, " ")}"
         traverse(curr_index + 1, end_index, cards, possible, Enum.at(possible, curr_index + 1))
       end
     end
@@ -18,12 +18,16 @@ defmodule Main do
 
   def add_cards(curr_index, cards_to_add, cards) do
     if cards_to_add == 0 do
+      
+      IO.puts "done #{Enum.join(cards, " ")}\n"
       cards
     else
 
       val = Enum.at(cards, curr_index) + 1
 
       new_cards = List.replace_at(cards, curr_index, val)
+      
+      IO.puts "new cards #{Enum.join(new_cards, " ")}"
 
       add_cards(curr_index + 1, cards_to_add - 1, new_cards)
     end
