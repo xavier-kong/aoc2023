199d4c299ab8196cf744e003b7c91dd57c78ab4d
xavier-kong <kongxianchengxavier@gmail.com>
2023-12-28 21:47:01 +0800

    perhaps rewrite is warranted

--- 04/part2.exs
+++ 04/part2.exs
@@ -1,8 +1,8 @@
 defmodule Main do
   def traverse(curr_index, end_index, cards, possible, cards_left) do
-    if curr_index >= end_index do
-      #IO.inspect("end")
-      #IO.inspect(cards)
+    if curr_index == end_index do
+      IO.inspect("end")
+      IO.inspect(cards)
       cards
     #else
       #if cards_left > 0 do
@@ -12,7 +12,7 @@ defmodule Main do
         #traverse(curr_index, end_index, new_cards, possible, cards_left - 1)
       else
         #IO.puts "cards_left == 0 #{Enum.join(cards, " ")}"
-        IO.puts " card left #{cards_left}"
+        IO.puts " card left #{cards_left} curr idx #{curr_index}"
         new_cards = Main.add_cards(curr_index + 1, Enum.at(possible, curr_index) * Enum.at(cards, curr_index), cards)
         traverse(curr_index + 1, end_index, new_cards, possible, Enum.at(possible, curr_index + 1) * Enum.at(new_cards, curr_index + 1))
         #traverse(curr_index + 1, end_index, cards, possible, Enum.at(possible, curr_index + 1) * Enum.at(cards, curr_index + 1))
@@ -21,12 +21,11 @@ defmodule Main do
   end
 
   def add_cards(curr_index, cards_to_add, cards) do
-    if cards_to_add == 0 do
-      
+    if cards_to_add == 0 or curr_index == length(cards) do
       IO.puts "done #{Enum.join(cards, " ")}\n"
       cards
     else
-
+      IO.puts "cards #{Enum.join(cards, "  ")} curr_index #{curr_index}"
       val = Enum.at(cards, curr_index) + 1
 
       new_cards = List.replace_at(cards, curr_index, val)
@@ -62,7 +61,7 @@ cards = List.duplicate(1, length(possible))
 
 IO.puts "traverse"
 
-res = Main.traverse(0, length(possible), cards, possible, Enum.at(possible, 0))
+res = Main.traverse(0, length(possible) - 1, cards, possible, Enum.at(possible, 0))
 
 sum = Enum.reduce(res, fn x, acc -> x + acc end)
 
