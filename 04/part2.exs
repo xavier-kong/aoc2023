c2f323cc8cfb7593c42226e28222fb18e56757cd
xavier-kong <kongxianchengxavier@gmail.com>
2023-12-29 22:32:17 +0800

    clean up

--- 04/part2.exs
+++ 04/part2.exs
@@ -1,37 +1,22 @@
 defmodule Main do
-  def traverse(curr_index, end_index, cards, possible, cards_left) do
+  def traverse(curr_index, end_index, cards, possible) do
     if curr_index == end_index do
-      IO.inspect("end")
-      IO.inspect(cards)
       cards
-    #else
-      #if cards_left > 0 do
-        #IO.puts "cards left #{cards_left} cards #{Enum.join(cards, " ")}"
-        #IO.puts "index #{curr_index}"
-        #new_cards = Main.add_cards(curr_index + 1, Enum.at(possible, curr_index), cards)
-        #traverse(curr_index, end_index, new_cards, possible, cards_left - 1)
-      else
-        #IO.puts "cards_left == 0 #{Enum.join(cards, " ")}"
-        IO.puts " card left #{cards_left} curr idx #{curr_index}"
-        new_cards = Main.add_cards(curr_index + 1, Enum.at(possible, curr_index) * Enum.at(cards, curr_index), cards)
-        traverse(curr_index + 1, end_index, new_cards, possible, Enum.at(possible, curr_index + 1) * Enum.at(new_cards, curr_index + 1))
-        #traverse(curr_index + 1, end_index, cards, possible, Enum.at(possible, curr_index + 1) * Enum.at(cards, curr_index + 1))
-      #end
+    else
+      cards_to_add = Enum.at(possible, curr_index) * Enum.at(cards, curr_index);
+
+      new_cards = Main.add_cards(curr_index + 1, cards_to_add, cards)
+
+      traverse(curr_index + 1, end_index, new_cards, possible)
     end
   end
 
   def add_cards(curr_index, cards_to_add, cards) do
     if cards_to_add == 0 or curr_index == length(cards) do
-      IO.puts "done #{Enum.join(cards, " ")}\n"
       cards
     else
-      IO.puts "cards #{Enum.join(cards, "  ")} curr_index #{curr_index}"
       val = Enum.at(cards, curr_index) + 1
-
       new_cards = List.replace_at(cards, curr_index, val)
-      
-      IO.puts "new cards #{Enum.join(new_cards, " ")}"
-
       add_cards(curr_index + 1, cards_to_add - 1, new_cards)
     end
   end
@@ -54,16 +39,10 @@ possible = File.stream!("test.txt")
   )
   |> Enum.to_list()
 
-
-IO.inspect(possible)
-
 cards = List.duplicate(1, length(possible))
 
-IO.puts "traverse"
-
-res = Main.traverse(0, length(possible) - 1, cards, possible, Enum.at(possible, 0))
+res = Main.traverse(0, length(possible) - 1, cards, possible)
 
 sum = Enum.reduce(res, fn x, acc -> x + acc end)
 
 IO.inspect(sum)
-
